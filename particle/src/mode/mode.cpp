803ec9387b25b12816180591d348b873b2916a6b
olaven <olav@sundfoer.com>
2019-12-07 10:51:38 +0100

    yesterday's code compiles with a few changes

--- particle/src/mode/mode.cpp
+++ particle/src/mode/mode.cpp
@@ -1,7 +1,6 @@
 #include "./mode.h"
 #include "../screen/layout.h"
 
-const int MAX_LAYOUT_COUNT = 8; 
 int next_button_pin = -1;
 int previous_button_pin = -1;
 
@@ -11,33 +10,22 @@ void listen_for_layout_change(LayoutState layout_state)
     //TODO: avoid multiple triggers (delay?)
 }
 
-int adjusted_layout_count(int layout_count)
-{
-    if (layout_count > MAX_LAYOUT_COUNT)
-    {
-        Serial.print("Last layouts are ommited because they exceed max count: ");
-        Serial.println(MAX_LAYOUT_COUNT);
-        layout_count = MAX_LAYOUT_COUNT;
-    }
-
-    return layout_count;
-}
 
 LayoutState setup_modes(
     int _next_button_pin, 
     int _previous_button_pin, 
     int total_layout_count, 
-    Layout layouts[MAX_LAYOUT_COUNT])  
+    Layout * layouts)  
 {
-    //NOTE: avoiding overflows later on 
-    total_layout_count = adjusted_layout_count(total_layout_count); 
 
     next_button_pin = _next_button_pin; 
-    previous_button_pin = _next_button_pin; 
+    previous_button_pin = _next_button_pin;
 
-    return {
-        .current_layout_index = 0, 
-        .total_layout_count = total_layout_count, 
+    struct LayoutState initial_state = {
+        .current_layout_index = 0,
+        .total_layout_count = total_layout_count,
         .layouts = layouts,
-    }; 
+    };
+    
+    return initial_state;
 }
--- particle/src/mode/mode.h
+++ particle/src/mode/mode.h
@@ -1,11 +1,12 @@
 #ifndef EMBEDDED_EXAM_MODE
 #define EMBEDDED_EXAM_MODE
 
+#include "../screen/layout.h" //TODO: can remove this once types are moved 
 struct LayoutState
 {
     int current_layout_index;
     int total_layout_count;
-    Layout layouts[MAX_LAYOUT_COUNT];
+    Layout * layouts;
 };
 
 //TODO functions currently in mode.cpp 
