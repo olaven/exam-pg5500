068e66842ec0c6fc9144d9b8e3b713145583083d
olaven <olav@sundfoer.com>
2019-12-16 00:53:30 +0100

    frontend renders data from particle

--- particle/src/layout/temperature/temperature.cpp
+++ particle/src/layout/temperature/temperature.cpp
@@ -3,6 +3,7 @@
 
 int inside_temperature_pin;
 int outside_temperature = -1; //updated via webhook integration
+int inside_temperature = -1; 
 String temperature_city = "Oslo"; //can be updated through functions
 
 void temperature_response_handler(const char *event, const char *data)
@@ -25,22 +26,25 @@ void setup_temperature()
 {
     Particle.subscribe("temperature_response", temperature_response_handler, MY_DEVICES); 
     Particle.variable("temperature_city", temperature_city); 
+    Particle.variable("outside_temperature", outside_temperature);
+    Particle.variable("inside_temperature", inside_temperature);
     Particle.function("change_temperature_city", change_temperature_city); 
 }
 
-int read_local_temperature() 
+void update_inside_temperature() 
 {
     const int analog_value = analogRead(inside_temperature_pin);
     const int celcius = (int)(80 * analog_value / 1024.0);
-    return celcius;
+    inside_temperature = celcius;
 }
 
 void updated_temperature_elements(Element elements[MAX_ELEMENT_COUNT]) 
 {
+    update_inside_temperature(); 
     Particle.publish("temperature", String::format("{ \"city\": \"%s\" }", temperature_city.c_str()));
 
-    elements[0] = {"local.", TOP_LEFT_CORNER}; 
-    elements[1] = {String(read_local_temperature()), TOP_RIGHT_CORNER};
+    elements[0] = {"inside.", TOP_LEFT_CORNER}; 
+    elements[1] = {String(inside_temperature), TOP_RIGHT_CORNER};
     elements[2] = {temperature_city, BOTTOM_LEFT_CORNER};
     elements[3] = {String(outside_temperature), BOTTOM_RIGHT_CORNER};
 }
